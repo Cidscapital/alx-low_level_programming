4d6e8e140a633fa9515c29477238a03586286035
Cidscaptal <jesse.jason2002@gmail.com>
2022-07-05 23:23:06 +0300

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {                  
-	char c[9] = "Developer";
+	char c[9] = "_putchar";
 	int i;
 
-	for (i = 0; i < 9; ++i)
+	for (i = 0; i < 8; ++i)
 		_putchar(c[i]);
 	_putchar('\n');
 	return (0);
